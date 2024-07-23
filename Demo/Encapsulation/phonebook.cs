4e5bfc028d1ec53d17209c5997ad6707dae6276d
Mohamed Elsayed <78699419+M-Elsayed9@users.noreply.github.com>
2024-07-23 13:42:38 +0300

    Encapsulation

---
+++ Demo/Encapsulation/phonebook.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo.Encapsulation
+{
+    internal struct Phonebook
+    {
+        #region Attributes
+        private string[] Names ;
+        private int[] Numbers;
+        private int size ;
+
+        #region Proprties
+
+        public int Size
+        {
+            get;
+        }
+        #endregion
+
+        #region Constructors
+        public Phonebook(int size)
+        {
+            this.Size = size;
+            Names = new string[size];
+            Numbers = new int[size];
+        }
+
+        #endregion
+        #endregion
+    }
+}
--- Demo/Program.cs
+++ Demo/Program.cs
@@ -1,10 +1,40 @@
-﻿namespace Demo
+﻿using Demo.Encapsulation;
+using System.Net.Http.Headers;
+
+namespace Demo
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region Encapsulation
+            //Employee emp = new Employee(1, "John Doe", 50000);
+
+            //Employee emp = new Employee(name: "John", id: 10, salary: 2000);
+
+            //Console.WriteLine(emp);
+
+            //emp.Id = 100; // set id directly through attributes
+            //Console.WriteLine(emp.Id); // get id directly through attributes
+
+            //Employee.name = "Mona"; // invalid after making it private
+            //emp.SetName("Mona");
+
+            //Console.WriteLine(emp.GetName());
+
+            //emp.Salary = 3000; // set salary using property as a setter 
+            //Console.WriteLine(emp.Salary); recomendded way to get salary using property as a getter
+            // as it easy to use 
+
+            //Employee emp = new Employee(10, "aliaa", 6000, 30);
+
+            //Console.WriteLine(emp);
+
+            //Console.WriteLine(emp.Deducation); 
+            #endregion
+
+            Phonebook phonebook = new Phonebook(20);       
+        
         }
     }
 }
