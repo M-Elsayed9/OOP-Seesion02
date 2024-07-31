b423eb3b5c98ee48e32e1910a51fe1670e108b73
Mohamed Elsayed <78699419+M-Elsayed9@users.noreply.github.com>
2024-07-31 08:21:18 +0300

    Part 02

---
+++ Assignment/Part02/Program.cs
@@ -0,0 +1,51 @@
+﻿namespace Assignment.Part02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Part 2
+
+            //Person[] Persons = new Person[3];
+
+            //Person person1 = new Person();
+            //person1.Name = "John";
+            //person1.Age = 25;
+
+            //Person person2 = new Person("Mohamed", 26);
+
+            //Person person3 = new Person("ali", 27);
+
+            //Persons[0] = person1;
+            //Persons[1] = person2;
+            //Persons[2] = person3;
+
+            //for (int i = 0; i < Persons.Length; i++)
+            //{
+            //    Console.WriteLine(Persons[i].ToString());
+            //}
+
+            //Person[] Persons = new Person[3];
+
+            //for (int i = 0; i < Persons.Length; i++)
+            //{
+            //    Console.WriteLine("Enter the name of the person: ");
+            //    string name = Console.ReadLine();
+            //    Console.WriteLine("Enter the age of the person: ");
+            //    int age = Convert.ToInt32(Console.ReadLine());
+            //    Persons[i] = new Person(name, age);
+            //}
+
+            //for (int i = 0; i < Persons.Length; i++)
+            //{
+            //    Console.WriteLine("Name: " + Persons[i].Name + " Age: " + Persons[i].Age);
+            //} 
+            #endregion
+
+            #region Part 3
+
+
+            #endregion
+        }
+    }
+}
